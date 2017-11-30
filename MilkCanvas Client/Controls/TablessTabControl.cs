9000fa72b64754b3bdac07de71b64ebf5453621c
Tristen Horton <tristen@tristenhorton.com>
2017-11-30 01:23:44 -0700

    a simple proc override for creating tabless tab controls
    used in the getting started window as a page system

---
+++ MilkCanvas Client/Controls/TablessTabControl.cs
@@ -0,0 +1,24 @@
+﻿namespace MilkCanvas.Controls
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Windows.Forms;
+
+    public class TablessTabControl : TabControl
+    {
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == 0x1328 && !this.DesignMode)
+            {
+                m.Result = (IntPtr)1;
+            }
+            else
+            {
+                base.WndProc(ref m);
+            }
+        }
+    }
+}
