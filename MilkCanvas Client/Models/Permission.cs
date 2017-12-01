4e3434ff910272685f08f8086716d07acd0f6b63
Tristen Horton <tristen@tristenhorton.com>
2017-12-01 01:10:51 -0700

    represents the permissions required for a command

---
+++ MilkCanvas Client/Models/Permission.cs
@@ -0,0 +1,27 @@
+﻿namespace MilkCanvas.Models
+{
+    using Enums;
+
+    using Newtonsoft.Json;
+
+    public class Permission
+    {
+        [JsonProperty]
+        private string command;
+
+        [JsonProperty]
+        private Group group;
+
+        public Permission(string command, Group group)
+        {
+            this.command = command;
+            this.group = group;
+        }
+
+        [JsonIgnore]
+        public string Command => command;
+
+        [JsonIgnore]
+        public Group Group => group;
+    }
+}
