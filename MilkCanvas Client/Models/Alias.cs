2012b1be6726f85b1e19f28c98035490ddc4a926
Tristen Horton <tristen@tristenhorton.com>
2017-12-01 01:10:26 -0700

    represents command aliases

---
+++ MilkCanvas Client/Models/Alias.cs
@@ -0,0 +1,25 @@
+﻿namespace MilkCanvas.Models
+{
+    using Newtonsoft.Json;
+
+    public class Alias
+    {
+        [JsonProperty]
+        private string command;
+
+        [JsonProperty]
+        private string alternate;
+
+        public Alias(string command, string alternate)
+        {
+            this.Command = command;
+            this.Alternate = alternate;
+        }
+
+        [JsonIgnore]
+        public string Command { get; }
+
+        [JsonIgnore]
+        public string Alternate { get; }
+    }
+}
