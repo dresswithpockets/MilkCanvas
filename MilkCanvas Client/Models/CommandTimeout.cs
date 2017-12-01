4d0d6c03a4213cc6ce29b34ee2279c7d68b16fe3
Tristen Horton <tristen@tristenhorton.com>
2017-12-01 01:11:12 -0700

    represents a command timeout set in place to stop spam

---
+++ MilkCanvas Client/Models/CommandTimeout.cs
@@ -0,0 +1,17 @@
+﻿namespace MilkCanvas.Models
+{
+    using System.Timers;
+
+    public class CommandTimeout
+    {
+        public CommandTimeout(string command, Timer timer)
+        {
+            this.Command = command;
+            this.Timer = timer;
+        }
+
+        public string Command { get; }
+
+        public Timer Timer { get; }
+    }
+}
