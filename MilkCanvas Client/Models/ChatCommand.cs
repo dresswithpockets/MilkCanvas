dbdeae4c0f0b87a6b0d9deb79f78df8dbd903c93
Tristen Horton <tristen@tristenhorton.com>
2017-12-01 01:10:40 -0700

    represents a relayed chat command

---
+++ MilkCanvas Client/Models/ChatCommand.cs
@@ -0,0 +1,25 @@
+﻿namespace MilkCanvas.Models
+{
+    using Newtonsoft.Json;
+
+    public class ChatCommand
+    {
+        [JsonProperty]
+        private string identifier;
+
+        [JsonProperty]
+        private string message;
+
+        public ChatCommand(string identifier, string message)
+        {
+            this.identifier = identifier;
+            this.message = message;
+        }
+
+        [JsonIgnore]
+        public string Identifier => this.identifier;
+
+        [JsonIgnore]
+        public string Message => this.message;
+    }
+}
